249407be9e1bef11c0ffda7c18a8ebb709518f10
GeetaSMath <geetasmath@gmail.com>
2021-12-06 18:12:26 +0530

    Added: connected database

--- src/main/java/com/bridgelabz/EmployeeConfig.java
+++ src/main/java/com/bridgelabz/EmployeeConfig.java
@@ -1,7 +1,24 @@
 package com.bridgelabz;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class EmployeeConfig {
-    public static void main(String[] args) {
-        System.out.println("Welcome to solve Problems");
+    public Connection getConfig() {
+        String dbURL = "jdbc:mysql://localhost:3306/employee_payroll_service";
+        String uname = "root";
+        String pass = "Geeta@123";
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(dbURL, uname, pass);
+            if (connection != null) {
+                System.out.println("Connected!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
---
+++ src/main/java/com/bridgelabz/EmployeePayroll.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class EmployeePayroll {
+    public static void main(String[] args) {
+        EmployeeConfig eConfig = new EmployeeConfig();
+        eConfig.getConfig();
+    }
+}
